3455446d37a95b60e6ee585cdec81ffd2b98842c
freddo <fred_solis@soy.sena.co>
2024-03-12 00:16:15 -0500

    creacion de la clase doctor

---
+++ Doctor.java
@@ -0,0 +1,12 @@
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+
+    //Comportamientos
+    public void showName(){
+        //esta metodo imprime el nombre del doctor
+        System.out.println(name);
+    }
+
+}
