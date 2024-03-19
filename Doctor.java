d355e9a5f021ba44171ee3affd6276c47a214ce0
freddo <fred_solis@soy.sena.co>
2024-03-19 00:34:05 -0500

    creando el UI y los metodos static

--- Doctor.java
+++ Doctor.java
@@ -1,17 +1,24 @@
 public class Doctor {
-    int id;
+    static int id=0;
     String name;
     String speciality;
 
     //construyendo el metodo constructor
     Doctor(){
         System.out.println("Construyendo el Objeto Doctor");
+        id++;
+    }
+    Doctor(String name){
+        System.out.println("El nombre del Doctor asiganado es: "+name);
     }
 
     //Comportamientos
     public void showName(){
         //esta metodo imprime el nombre del doctor
         System.out.println(name);
+    }
+    public void showId(){
+        System.out.println("ID Doctor: "+ id);
     }
 
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
+import static UI.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
         Doctor myDoctor= new Doctor();
         myDoctor.name="Fred Willians Solis";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctorAnn= new Doctor();
+        myDoctor.name="Caro Cruz";
+        myDoctor.showName();
+        myDoctor.showId();
+
+        showMenu();
     }
 }
